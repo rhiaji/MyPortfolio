dc0a46cf4b067bde327dd4f8d0bef1c1a02aa649
rhiaji <mrdavemariano@gmail.com>
2023-10-24 21:11:20 +0800

    first commit

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+document.getElementById("aboutButton").addEventListener("click", function () {
+  document.getElementById("about").scrollIntoView({behavior: "smooth"})
+})
+
+document.getElementById("skillsButton").addEventListener("click", function () {
+  document.getElementById("skills").scrollIntoView({behavior: "smooth"})
+})
+
+document.getElementById("projectsButton").addEventListener("click", function () {
+  document.getElementById("projects").scrollIntoView({behavior: "smooth"})
+})
+
+document.getElementById("hiremeButton").addEventListener("click", function () {
+  document.getElementById("hireme").scrollIntoView({behavior: "smooth"})
+})
